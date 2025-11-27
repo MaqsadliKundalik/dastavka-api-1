78e6630107b6deddbf8c5b2ea6bc894c30fa4de6
Odilbek <=>
2025-11-27 13:16:38 +0500

    c

--- dastavka/settings.py
+++ dastavka/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%cm#r9=796&ze7jzuxpw-@b8xi&$fsh+0n2y-+nq2%hk&k2ax2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
