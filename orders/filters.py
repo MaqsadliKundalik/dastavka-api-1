94b8de906da2b0de68953a19f3720f9de67b8146
Odilbek <=>
2025-11-29 10:00:24 +0500

    c

--- orders/filters.py
+++ orders/filters.py
@@ -7,8 +7,8 @@ class OrderFilter(django_filters.FilterSet):
     Buyurtmalar uchun murakkab filterlash
     """
     
-    # Client ID bo'yicha qidirish (to'liq ID yoki qismiy)
-    client_id = django_filters.CharFilter(method='filter_client_id')
+    # Client ID bo'yicha faqat to'liq moslikda qidirish
+    client_id = django_filters.NumberFilter(field_name='client__id')
     
     # Client nomi bo'yicha qidirish
     client_name = django_filters.CharFilter(field_name='client__full_name', lookup_expr='icontains')
@@ -20,15 +20,7 @@ class OrderFilter(django_filters.FilterSet):
         model = Order
         fields = ['status', 'assigned_to', 'created_by']
     
-    def filter_client_id(self, queryset, name, value):
-        """
-        Client ID bo'yicha filterlash - to'liq ID yoki qismiy ID qidirish
-        Masalan: ID=12345 bo'lsa, '123' kiritganda ham topadi
-        """
-        if value:
-            # ID'si qismiy mos keluvchi clientlarni qidirish
-            return queryset.filter(client__id__icontains=value)
-        return queryset
+    # endi custom filter kerak emas, NumberFilter exact moslik uchun yetarli
 
 
 class ClientFilter(django_filters.FilterSet):
