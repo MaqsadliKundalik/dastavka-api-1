2875fc4aa9ff9719ccdc3160bc17461aea36904a
Odilbek <=>
2026-03-03 22:33:26 +0500

    c

--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,15 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .models import Note
 from .serializers import NoteSerializer
 
 class NoteListCreateView(generics.ListCreateAPIView):
     queryset = Note.objects.all().order_by('-created_at')
     serializer_class = NoteSerializer
+    permission_classes = [AllowAny]
 
 class NoteRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = [AllowAny]
